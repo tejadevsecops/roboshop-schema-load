208c828ab1035292c6a633f64b02e8555547a75d
Teja <awstheja727@gmail.com>
2025-02-03 22:17:48 +0530

    Roboshop schema load docker file changes

--- run.sh
+++ run.sh
@@ -32,9 +32,11 @@ if [ "${DB_TYPE}" == "mysql" ]; then
 fi
 
 if [ "${DB_TYPE}" == "mongo" ]; then
-  git clone ${APP_GIT_URL} .
+  #git clone ${APP_GIT_URL} .
+  cd roboshop-catalogue
   mongosh --host ${DB_HOST} <${SCHEMA_FILE}
 elif [ "${DB_TYPE}" == "mysql" ]; then
-  git clone ${APP_GIT_URL} .
+#  git clone ${APP_GIT_URL} .
+  cd roboshop-shipping
   mysql -h $DB_HOST -u$DB_USER -p$DB_PASS <${SCHEMA_FILE}
 fi
